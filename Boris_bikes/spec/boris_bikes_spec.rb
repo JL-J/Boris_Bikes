29314c5df44379ef4f0a551c1f37de5143788884
NaomiBid <naomi0bid@gmail.com>
2018-10-02 14:41:16 +0100

    First rspec test written and passed

--- Boris_bikes/spec/boris_bikes_spec.rb
+++ Boris_bikes/spec/boris_bikes_spec.rb
@@ -1,5 +1,7 @@
 require 'boris_bikes.rb'
 
 describe DockingStation do
-
+  it 'releases bike when when the mothod release_bike is called' do
+    expect(DockingStation.new.respond_to?(:release_bike)).to eq true
+  end
 end
