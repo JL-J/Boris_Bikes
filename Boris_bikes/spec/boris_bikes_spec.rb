26980b9c0ea3c231b81b884625be4ecc41302807
NaomiBid <naomi0bid@gmail.com>
2018-10-02 15:37:56 +0100

    written and passed DockingStation second spec test

--- Boris_bikes/spec/boris_bikes_spec.rb
+++ Boris_bikes/spec/boris_bikes_spec.rb
@@ -4,4 +4,7 @@ describe DockingStation do
   it 'releases bike when when the mothod release_bike is called' do
     expect(DockingStation.new.respond_to?(:release_bike)).to eq true
   end
+  it 'gets a bike' do
+    expect(DockingStation.new.release_bike.working?).to eq true
+  end
 end
