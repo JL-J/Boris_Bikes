556596a733734f09d2188ae77d64421823bfa57e
NaomiBid <naomi0bid@gmail.com>
2018-10-02 16:55:28 +0100

    added initialize method

--- Boris_bikes/lib/docking_station.rb
+++ Boris_bikes/lib/docking_station.rb
@@ -1,13 +1,19 @@
 require_relative 'boris_bikes'
 
 class DockingStation
+  attr_reader :station
+
+  def initialize
+    @station = []
+  end
+
 
   def release_bike
     Bike.new
   end
 
-  def dock
-
+  def dock(bike)
+    @station.push(bike)
   end
 
 end
